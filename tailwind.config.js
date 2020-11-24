f7eccf42a0af66c6340fb3dc18c2cecbe847c109
Joel Hooks <joelhooks@gmail.com>
2020-11-24 14:21:12 -0800

    upgrade to tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   purge: ['./src/**/*.tsx'],
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1440px',
-      '3xl': '1920px',
-    },
     typography: (theme) => ({}),
     extend: {},
   },
