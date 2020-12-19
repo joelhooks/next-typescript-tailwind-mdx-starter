08417b8e6ca105bf9edf2906207e03af67b1199a
Zac Jones <zacjones93@gmail.com>
2020-12-19 17:07:17 -0500

    update copy on page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,10 +5,8 @@ export const About = () => {
         <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
           <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">Data to enrich your</span>
-              <span className="block text-indigo-600 xl:inline">
-                online business
-              </span>
+              <span className="block xl:inline">About </span>
+              <span className="block text-indigo-600 xl:inline">Me</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
